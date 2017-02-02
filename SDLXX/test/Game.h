1ce7dabd1ee256c93db6a5cd6103ca882adc5a8b
Egor Makarenko <egormkn@yandex.ru>
2017-02-03 01:08:22 +0300

    Physics!!!

--- SDLXX/test/Game.h
+++ SDLXX/test/Game.h
@@ -28,70 +28,74 @@ namespace SDLXX {
 
 
             window = &w;
-            // Init resources
 
-            // Define the gravity vector.
-            /*gravity = new b2Vec2(0.0f, -10.0f);
+            b2Vec2 gravity(0.0f, -9.8f);
+            world = std::make_unique<b2World>(gravity);
 
-            // Construct a world object, which will hold and simulate the rigid bodies.
-            world = new b2World(*gravity);
+
+            /*b2BodyDef groundBodyDef;
+            groundBodyDef.position.Set(0.0f, -10.0f);
+            b2Body *groundBody = m_world->CreateBody(&groundBodyDef);
+            b2PolygonShape groundBox;
+            groundBox.SetAsBox(50.0f, 10.0f);
+
+            groundBody->CreateFixture(&groundBox, 0.0f);*/
+
+
 
             // Define the ground body.
-            groundBodyDef = new b2BodyDef;
-            groundBodyDef->position.Set(0.0f, -10.0f);
+            b2BodyDef groundBodyDef;
+            groundBodyDef.position.Set(0.0f, -10.0f);
 
             // Call the body factory which allocates memory for the ground body
             // from a pool and creates the ground box shape (also from a pool).
             // The body is also added to the world.
-            groundBody = world->CreateBody(groundBodyDef);
+            b2Body *groundBody = world->CreateBody(&groundBodyDef);
 
             // Define the ground box shape.
-            groundBox = new b2PolygonShape;
+            b2PolygonShape groundBox;
 
             // The extents are the half-widths of the box.
-            groundBox->SetAsBox(50.0f, 10.0f);
+            groundBox.SetAsBox(50.0f, 10.0f);
 
             // Add the ground fixture to the ground body.
-            groundBody->CreateFixture(groundBox, 0.0f);
+            groundBody->CreateFixture(&groundBox, 0.0f);
+
+
 
             // Define the dynamic body. We set its position and call the body factory.
-            bodyDef = new b2BodyDef;
-            bodyDef->type = b2_dynamicBody;
-            bodyDef->position.Set(0.0f, 20.0f);
-            body = world->CreateBody(bodyDef);
+            b2BodyDef bodyDef;
+            bodyDef.type = b2_dynamicBody;
+            bodyDef.position.Set(0.0f, 10.0f);
+            body = world->CreateBody(&bodyDef);
 
             // Define another box shape for our dynamic body.
-            dynamicBox = new b2PolygonShape;
-            dynamicBox->SetAsBox(1.0f, 1.0f);
+            b2PolygonShape dynamicBox;
+            dynamicBox.SetAsBox(1.0f, 1.0f);
 
             // Define the dynamic body fixture.
-            fixtureDef = new b2FixtureDef;
-            fixtureDef->shape = dynamicBox;
+            b2FixtureDef fixtureDef;
+            fixtureDef.shape = &dynamicBox;
 
             // Set the box density to be non-zero, so it will be dynamic.
-            fixtureDef->density = 1.0f;
+            fixtureDef.density = 1.0f;
 
             // Override the default friction.
-            fixtureDef->friction = 0.3f;
+            fixtureDef.friction = 0.3f;
 
             // Add the shape to the body.
-            body->CreateFixture(fixtureDef);
+            body->CreateFixture(&fixtureDef);
 
             // Prepare for simulation. Typically we use a time step of 1/60 of a
             // second (60Hz) and 10 iterations. This provides a high quality simulation
             // in most game scenarios.
-            float32 timeStep = 1.0f / 60.0f;*/
+            float32 timeStep = 1.0f / 60.0f;
 
         }
 
         void onDestroy() override {
             Log::log("[" + getTitle() + "] Scene destroyed");
             // Free resources
-            SDL_DestroyTexture(image);
-            image = nullptr;
-
-
-
 
 
             /*delete world;
@@ -126,28 +130,34 @@ namespace SDLXX {
                     //runIntent(new Menu("New title"));
                     // finish();
                     // My changes
+                } else if(e.getEvent().key.keysym.sym == SDLK_SPACE) {
+                    if(e.getEvent().key.repeat == 0) {
+                        Log::log("JUMP");
+                        body->ApplyLinearImpulse( b2Vec2(0,20), body->GetWorldCenter(), true );
+                    }
                 } else {
                     finish();
                 }
             }
-            if(e.getType() == SDL_WINDOWEVENT) {
-                window->getRenderer().render();
-            }
         }
 
         void update(Uint32 t, Uint32 dt) override {
-            //world->Step(((float32) dt) / 1000.0f, 6, 2);
+            world->Step(((float32) dt) / 1000.0f, 6, 2);
+            pos++;
+            if(pos > 300) {
+                pos = 0;
+            }
         }
 
         void render(Renderer &renderer) override {
             renderer.setColor(Color(0xFFFFFFFF));
             renderer.clear();
 
-            /*b2Vec2 position = body->GetPosition();
-            float32 angle = body->GetAngle();*/
+            b2Vec2 position = body->GetPosition();
+            float32 angle = body->GetAngle();
 
             SDL_Rect fillRect = {
-                    (int) (position.x * 10), 10, 100, 100
+                    10, 201 - (int) (position.y * 20), 100, 100
             };
 
             renderer.setColor(Color(0xFF8888FF));
@@ -157,12 +167,10 @@ namespace SDLXX {
 
     private:
         int pos = 0;
-        bool loaded = false;
-        std::vector<Object *> objects;
-        SDL_Texture *image = nullptr;
         Window *window = nullptr;
 
-
+        std::unique_ptr<b2World> world;
+        b2Body *body = nullptr;
 
         /*b2Vec2 *gravity = nullptr;
         b2World *world = nullptr;
