9362c18ced70729f1319e3deedd270d24f0129ee
Egor Makarenko <egormkn@yandex.ru>
2019-10-11 23:42:55 +0300

    Implement Timer

---
+++ src/core/Timer.cpp
@@ -0,0 +1,27 @@
+#include <SDL_timer.h>
+#include <sdlxx/core/Timer.h>
+
+using namespace sdlxx::core;
+
+uint32_t Timer::getTicks() { return SDL_GetTicks(); }
+
+uint64_t Timer::getPerformanceCounter() { return SDL_GetPerformanceCounter(); }
+
+uint64_t Timer::getPerformanceFrequency() {
+  return SDL_GetPerformanceFrequency();
+}
+
+void Timer::delay(uint32_t milliseconds) { SDL_Delay(milliseconds); }
+
+Timer::Timer(uint32_t interval, Callback callback, void* param) {
+  SDL_TimerCallback timer_callback = *callback.target<SDL_TimerCallback>();
+  id = SDL_AddTimer(interval, timer_callback, param);
+  if (id == 0) {
+    throw std::runtime_error("Failed to create a timer: " +
+                             std::string(SDL_GetError()));
+  }
+}
+
+Timer::~Timer() {
+  bool removed = SDL_RemoveTimer(id);
+}
